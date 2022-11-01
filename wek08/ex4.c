6ee86c6875bcb52441056833bfedee4ecaf0ac91
Niks Gont <88322931+niksgont@users.noreply.github.com>
2022-11-01 23:05:04 +0300

    Create ex4.c

---
+++ wek08/ex4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+int main() {
+    void *ptr[10];
+    struct rusage res;
+    for (int i = 0; i < 10; i++) {
+        ptr[i] = malloc(10 * 1024 * 1024);
+        if (ptr[i] != NULL)
+            memset(ptr[i], 0, 10 * 1024 * 1024);
+
+        getrusage(RUSAGE_SELF, &res);
+        sleep(1);
+    }      
+    return 0;
+}
